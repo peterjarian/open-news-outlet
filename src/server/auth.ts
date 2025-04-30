6d2628320bf88272fe7b40478b4cd2f26f203fbb
Peter van der Heijden <contact@peterjarian.dev>
2025-04-30 17:31:44 +0200

    feat(better-auth): added configuration

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,4 +1,5 @@
 import { betterAuth } from 'better-auth';
+import { magicLink } from 'better-auth/plugins';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '@/server/db';
 import 'server-only';
@@ -6,7 +7,21 @@ import 'server-only';
 export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
+        requireEmailVerification: true, // make sure users need to verify their email before signing in
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        sendResetPassword: async ({ user, url, token }, request) => {},
     },
+    emailVerification: {
+        sendOnSignUp: true,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        sendVerificationEmail: async ({ user, url, token }, request) => {},
+    },
+    plugins: [
+        magicLink({
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            sendMagicLink: async ({ email, token, url }, request) => {},
+        }),
+    ],
     database: drizzleAdapter(db, {
         provider: 'pg',
     }),
